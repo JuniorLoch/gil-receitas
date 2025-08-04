d743f3978fb51ce5b85a9d18458e2db5d2f57d1e
JuniorLoch <juninho_1loch@hotmail.com>
2025-08-04 17:02:49 -0400

    🎉 config inicial

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import prettier from 'eslint-plugin-prettier';
+import tsParser from '@typescript-eslint/parser';
+import ts from '@typescript-eslint/eslint-plugin';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,8 +12,48 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+const TS_FILES_REGEX = '**/{*.ts,*.md/*.ts}';
+const TSX_FILES_REGEX = '**/{*.tsx,*.md/*.tsx}';
+
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    files: [TS_FILES_REGEX, TSX_FILES_REGEX],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: { modules: true },
+        ecmaVersion: 'latest',
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': ts,
+      prettier: prettier,
+      ts,
+    },
+    settings: {
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx'],
+      },
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+        },
+        node: {
+          extensions: ['.ts', '.tsx', '.mjs'],
+        },
+      },
+    },
+    rules: {
+      ...prettier.configs.recommended.rules,
+      'react-hooks/exhaustive-deps': 'off',
+      'react/no-unescaped-entities': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      'newline-before-return': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+    },
+  },
 ];
 
 export default eslintConfig;
