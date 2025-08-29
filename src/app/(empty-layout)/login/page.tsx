05ac98c6ea9a7dc73e260b27a31b8a1baf0b31f6
JuniorLoch <juninho_1loch@hotmail.com>
2025-08-29 09:39:48 -0400

    ♻️ Testando import de imagem estático

--- src/app/(empty-layout)/login/page.tsx
+++ src/app/(empty-layout)/login/page.tsx
@@ -4,8 +4,7 @@ import { GilGrid } from '@/app/(dashboard)/components/GilGrid'
 import { ColorModeButton } from '@/app/components/color-mode'
 import { AspectRatio, GridItem } from '@chakra-ui/react'
 import { FormLogin } from './FormLogin'
-import bannerLoginImageLight from '@images/banner-paes-light.png'
-import bannerLoginImageDark from '@images/banner-paes-dark.png'
+
 import NextImage from 'next/image'
 
 export default function LoginPage() {
@@ -15,10 +14,22 @@ export default function LoginPage() {
       <GilGrid minH={'100vh'} gap={0}>
         <GridItem colSpan={8}>
           <AspectRatio h={'100%'} ratio={16 / 9} _dark={{ display: 'none' }}>
-            <NextImage src={bannerLoginImageLight} alt='Imagem do banner de login' priority />
+            <NextImage
+              src={'/images/banner-paes-light.png'}
+              alt='Imagem do banner de login'
+              width={1024}
+              height={1024}
+              priority
+            />
           </AspectRatio>
           <AspectRatio h={'100%'} ratio={16 / 9} _light={{ display: 'none' }}>
-            <NextImage src={bannerLoginImageDark} alt='Imagem do banner de login' priority />
+            <NextImage
+              src={'/images/banner-paes-dark.png'}
+              alt='Imagem do banner de login'
+              width={1024}
+              height={1024}
+              priority
+            />
           </AspectRatio>
         </GridItem>
         <GridItem colSpan={4}>
